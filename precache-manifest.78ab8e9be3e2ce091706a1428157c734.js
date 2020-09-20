6e3c65a1fbcc548016b9e481ad4eb838ae7b9971
Sergo58 <godfather59@yandex.ru>
2020-09-21 00:39:06 +0500

    Updates

---
+++ precache-manifest.78ab8e9be3e2ce091706a1428157c734.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "919576063fe5765320f8e6b20bcf259d",
+    "url": "/Counter/index.html"
+  },
+  {
+    "revision": "9cfe0feb490419896e0d",
+    "url": "/Counter/static/css/main.128bdad9.chunk.css"
+  },
+  {
+    "revision": "094dc2bdccf2deeb7427",
+    "url": "/Counter/static/js/2.e2f82d03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Counter/static/js/2.e2f82d03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cfe0feb490419896e0d",
+    "url": "/Counter/static/js/main.1c9028d5.chunk.js"
+  },
+  {
+    "revision": "8290f2e6b6e6218d3651",
+    "url": "/Counter/static/js/runtime-main.c5ebaff2.js"
+  }
+]);
